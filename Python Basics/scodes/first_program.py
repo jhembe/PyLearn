f1a39c76eb0e1783f3bac4548422e89d203b7cdb
jhembe <jhembe202@gmail.com>
2023-03-31 17:38:31 +0300

    First commit on the repo. Functional files

---
+++ Python Basics/scodes/first_program.py
@@ -0,0 +1,12 @@
+# This program says hello and asks for my name
+
+print('Hello World')
+print("What's your name?") # asks for the user's name
+
+myName = input();
+print("It's good to meet you, "+myName);
+print('The length of your name is : ' );
+print(len(myName))
+
+my_age = input("What's your age ?");
+print('You will be ' + str(int(my_age) +1) + ' in a year.');
